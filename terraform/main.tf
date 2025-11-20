068a467969612c104f7e34abc4d1a724f5e8889b
Steve McGhee <smcghee@google.com>
2025-11-20 11:39:42 -0800

    Increase node pool size to 2 for more CPU capacity

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
   location   = var.zone
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
